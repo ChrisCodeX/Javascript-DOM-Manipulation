cbbae7bd49f78b07c83d3e83e77ad5f4f5c91e61
Christian <c.espinoza.code@gmail.com>
2022-12-10 19:39:42 -0500

    comments changed in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 console.log('Test 125')
 
 /* appendChild */
-// Here we add a ´h3´ element in the container
+
+// Here we save the container using query selector
 const container = document.querySelector('section.namesSection')
 
+// Here we create the h3 element
 const h3 = document.createElement('h3')
 
+// Here we add a ´h3´ element in the container
 if (container != null) {
     container.appendChild(h3)
 }
 
+/* createTextNode */
+//
+const texto = document.createTextNode('Texto agregado')
+
+if (container != null) {
+    container.appendChild(texto)
+}
